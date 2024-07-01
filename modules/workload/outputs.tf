16ba563edb636ea2470ddf6338652bb316baa799
Jared Holgate <jaredholgate@microsoft.com>
2024-07-01 11:41:28 +0100

    Fix linting

--- modules/workload/outputs.tf
+++ modules/workload/outputs.tf
@@ -1,11 +1,16 @@
 output "resource" {
   description = "resource Id output"
-  value       = azurerm_backup_policy_vm_workload.this
+  value       = var.workload_policy == null ? null : azurerm_backup_policy_vm_workload.this[0]
 }
 
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource_id" {
   description = "resource Id output"
-  value       = azurerm_backup_policy_vm_workload.this.id
+  value       = var.workload_policy == null ? null : azurerm_backup_policy_vm_workload.this[0].id
+}
+
+output "output_protection_policy" {
+  description = "The output protection policy"
+  value = local.backup
 }
