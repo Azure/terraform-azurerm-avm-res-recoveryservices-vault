1d47c01fe7656c0db166a2c67201417dced4c487
CABREG0 <CABREG0@microsoft.com>
2025-05-01 01:58:43 +0000

    fix: remove azapi provider requirement from example configuration

--- examples/04-vault-backup-policies/terraform.tf
+++ examples/04-vault-backup-policies/terraform.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
-    azapi = {
-      source  = "Azure/azapi"
-      version = ">= 1.0.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.107.0"
