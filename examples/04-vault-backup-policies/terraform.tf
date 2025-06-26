5d47c3e5985e02656cf3e73ae3d36dca51d5c4c9
CABREG0 <CABREG0@microsoft.com>
2025-06-26 15:29:31 -0700

    chore: update provider version constraints for compatibility

--- examples/04-vault-backup-policies/terraform.tf
+++ examples/04-vault-backup-policies/terraform.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.9, < 2.0"
 
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = ">= 1.0.0, < 2.0.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.34.0, < 5.0.0"
