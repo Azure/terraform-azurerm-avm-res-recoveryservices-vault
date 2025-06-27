3b19afef99fd8e8c647344fdc3faf78bf1f4b52b
CABREG0 <CABREG0@microsoft.com>
2025-06-26 18:11:16 -0700

    chore: update provider version constraints for compatibility

--- terraform.tf
+++ terraform.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.9, < 2.0"
 
   required_providers {
-    azapi = {
-      source  = "Azure/azapi"
-      version = ">= 1.14.0, < 3.0.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.116.0, < 5.0.0"
