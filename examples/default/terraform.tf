aa4fc2477226c4db5a746b94b87890af8e27e573
CABREG0 <CABREG0@microsoft.com>
2026-06-23 08:36:07 -0700

    fix: remove azurerm backend configuration from default example

--- examples/default/terraform.tf
+++ examples/default/terraform.tf
@@ -1,14 +1,6 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
 
-  backend "azurerm" {
-    subscription_id      = "c5c1228d-b650-4f0a-97ea-1f8cfdc417c5"
-    resource_group_name  = "rg-tfstate-shared"
-    storage_account_name = "stb7q2znqmpox3hmix"
-    container_name       = "tfstate"
-    key                  = "default.tfstate"
-  }
-
   required_providers {
     azapi = {
       source  = "Azure/azapi"
